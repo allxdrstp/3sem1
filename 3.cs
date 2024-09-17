60ae65cd00b85ad9f6db51ddc202c8c0eaf566c1
allxdrstp <145099647+allxdrstp@users.noreply.github.com>
2024-09-18 00:21:20 +0300

    Update and rename 3 to 3.cs

--- 3.cs
+++ 3.cs
@@ -54,9 +54,9 @@ class Program
 {
     static void Main()
     {
-        Point p1 = new Point(0, 0);
-        Point p2 = new Point(4, 0);
-        Point p3 = new Point(4, 3);
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(3, 0);
+        Point p3 = new Point(4, 5);
 
         Figure triangle = new Figure(p1, p2, p3) { Name = "Triangle" };
 
